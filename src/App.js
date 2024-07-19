27cfac688e313f92a97fd5c80779ab42039d7b72
unknown <arpitregmi121@gmail.com>
2024-07-19 21:17:48 +0545

    lots of functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar.js";
 import TextForm from "./Components/TextForm.js";
+import Alert from "./Components/Alert.js";
+import About from "./Components/about.js";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#31363F";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
   return (
     <>
-      {/* navbar component*/}
-      <Navbar title="TextPros" />
-      <div className="container my-3">
-        <TextForm heading="Your text to Be Modified" />
-      </div>
+      <Router>
+        {/* navbar component*/}
+        <Navbar title="TextPros" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About mode={mode} />
+            </Route>
+            <Route exact path="/">
+              <TextForm showAlert={showAlert} heading="Your text to Be Modified" mode={mode} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -2,41 +2,104 @@ import React, { useState } from "react";
 
 export default function TextForm(props) {
   const handleUpCase = () => {
-    console.log("upper case");
     let newText = text.toUpperCase();
     setText(newText);
+    props.showAlert("Text Uppercased", "success");
   };
 
   const handleLowCase = () => {
-    console.log("lower case");
     let newText = text.toLowerCase();
     setText(newText);
+    props.showAlert("Text Lowercased", "success");
+  };
+  const handleClear = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text Cleared", "success");
+  };
+  const handleSenUpCase = () => {
+    let firstLetter = text.charAt(0).toUpperCase();
+    let remainingLetter = text.slice(1, text.length);
+    let newText = firstLetter.concat(remainingLetter);
+    setText(newText);
+    props.showAlert("Text Sentence Uppercased", "success");
+  };
+  const handlerev = () => {
+    let newText = text.split("").reverse().join("");
+    // split is need here to make a array of every single letter reverse() reverses the array and join make the array into string
+    setText(newText);
+    props.showAlert("Text Reversed", "success");
+  };
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+    props.showAlert("Text to speech activated", "success");
   };
   const handleOnChange = (event) => {
-    console.log("on change");
     setText(event.target.value);
   };
+  const handleExtraSpace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra Space Removed", "success");
+  };
 
-  const [text, setText] = useState("Enter your text here");
+  const wordCounter = () => {
+    let newText = text.split(/[ ]+/).join(" ");
+    let newText2 = newText.split(" ").length;
+
+    return newText2;
+  };
+
+  const [text, setText] = useState("");
 
   return (
-    <div>
-      {/* heading */}
-      <h1>{props.heading}</h1>
+    <>
+      <div className="container" style={{ color: props.mode === "light" ? "black" : "white" }}>
+        {/* heading */}
+        <h1>{props.heading}</h1>
 
-      <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="10" value={text} onChange={handleOnChange} />
+        <div className="mb-3">
+          <textarea className="form-control" id="myBox" rows="10" style={{ backgroundColor: props.mode === "light" ? "white" : "#31363F", color: props.mode === "light" ? "black" : "white" }} value={text} onChange={handleOnChange} />
+        </div>
+
+        {/* uppercase button */}
+        <button className="btn btn-primary mx-1" onClick={handleUpCase}>
+          Convert to UpperCase
+        </button>
+
+        {/* lower case button */}
+        <button className="btn btn-primary mx-1" onClick={handleLowCase}>
+          Convert to LowerCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleClear}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleSenUpCase}>
+          Setence Uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={speak}>
+          Read text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handlerev}>
+          Reverse text
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleExtraSpace}>
+          Clear Extra Space
+        </button>
       </div>
+      <div className="container my-3" style={{ color: props.mode === "light" ? "black" : "white" }}>
+        <h2>Your text Summary</h2>
+        <p>
+          {text.length > 0 ? wordCounter() : "0"} words, {text.length} characters
+        </p>
+        <p>{text.split(" ").length * 0.008} minutes to read this</p>
 
-      {/* uppercase button */}
-      <button className="btn btn-primary " onClick={handleUpCase}>
-        Convert to UpperCase
-      </button>
-
-      {/* lower case button */}
-      <button className="btn btn-primary mx-3" onClick={handleLowCase}>
-        Convert to LowerCase
-      </button>
-    </div>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text.split(/[ ]+/).join(" ") : "Enter the text above to preview it"}</p>
+        <p>{console.log}</p>
+      </div>
+    </>
   );
 }
